a21f994afe1aa7e81fd208b383f7224bf82b6647
Anam Khan <anamzoakhan@gmail.com>
2020-10-09 01:46:49 +0530

    saving updated pic in database

--- server/routes/user.js
+++ server/routes/user.js
@@ -22,9 +22,12 @@ router.get('/user/:id', requireLogin, (req,res) => {
   })
 })
 
-router.put('/updatepic', (req, res) => {
-  User.findByIdAndUpdate(req.user._id, {$set:{pic:req.body.pic}}, (err,result) => {
-    
+router.put('/updatepic', requireLogin, (req, res) => {
+  User.findByIdAndUpdate(req.user._id, {$set:{pic:req.body.pic}}, {new:true}, (err,result) => {
+    if(err) {
+      return res.status(422).json({error: "Picture cannot be posted"})
+    }
+    res.json(result)
   }) 
 })
 
